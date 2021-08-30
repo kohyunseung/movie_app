5a3619b2a8944a99a400fc2aa4d3d023946db014
kohyunseung <hyun910625@naver.com>
2021-08-30 22:33:43 +0900

    #2 last commit

--- src/App.js
+++ src/App.js
@@ -1,64 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function Food({name, image, rating}){
-    
-    return(
-        <div>
-        <h2>I Like {name}</h2>
-        <h4>{rating}/5</h4>
-        <img src={image} alt={name}></img>
-        </div>
-    );
-}
+class App extends React.Component{
+    state = {
+        isLoading: true,
+        movies: []
+    }
+    getMovies = async () => {
+        const {data: {data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+        this.setState({movies, isLoading: false});
+    }
 
-const foodLike = [
-    {
-        "id": 1,
-        "name" : "kimbap",
-        "image" : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA4MDNfNzIg%2FMDAxNjI3OTkxMTE4MzE2.o8tHGXNIWG5wmsF4W7TjtZBtEONrTvVPD7uSPzdzXhAg.FrkciiKW_TK-CtWB2sIWpvYVwNzT3Ns9PZsgbbr37oAg.JPEG.cooklhj%2FIMG_6588.JPG&type=a340"
-        , "rating": 5
-    },
-    {
-        "id": 2,
-        "name" : "ramen",
-        "image" : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA4MTRfMjE1%2FMDAxNjI4OTI1MzAyNTky.UOSPvQqSvig8Q5h0rjUBdty1gOly4oRQRkrnF_8lR1og.05g0lSo6Pt2X2_Ssjo8Az348jFgF3nv_tKWouwoMahUg.JPEG.tangkwon%2F021.jpg&type=a340",
-        "rating": 4.5
-    },
-    {
-        "id": 3,
-        "name" : "ttokbokki",
-        "image" : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA4MDhfOCAg%2FMDAxNjI4MzkxNzU2MjQ2.guIIVwh8hpHdpykL6p6W-WHevEaJAaYzFn8D_64U084g.HNhnI07IALlnbex5G5i2XX2iLHLLkqO6CY2e000Cb6Ag.PNG.ssayumisky%2FIMG_7003.png&type=a340",
-        "rating": 4.7
-    },
-    {
-        "id": 4,
-        "name" : "curry",
-        "image" : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA3MzFfODMg%2FMDAxNjI3NjYwMzEyMjQ4.joYpi153Ipq2sSsctdJtfgQF06euddaBSqAp_DGikeYg.O-d--CuwqQ05vCYGANXaichjaMU_5eA4IxSSW0Qa-bsg.JPEG.bibi313%2F20210722_133756.jpg&type=a340",
-        "rating": 3.6
-    },
-    {
-        "id": 5,
-        "name" : "meat",
-        "image" : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA4MDVfOTgg%2FMDAxNjI4MTczNzQzMjAy.zYd1zoUnnC-IdFlxAFCZ1ySljWZAgxnExexyPk0q-m0g.poBAkfv5S-UbzOXcCwF6GXf4oJVIuCAX3Fa993nbOcsg.PNG.yooonpig%2Fimage.png&type=a340",
-        "rating": 4.8
-    },
-];
-
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired
-}
-
-function App() {
-  return (
-    <div className="App">
-        {foodLike.map((food) => {
-            return <Food key={food.id} name={food.name} image={food.image} rating={food.rating}/>;
-        })}
-    </div>
-  );
+    componentDidMount(){
+        this.getMovies();
+    }
+    render(){
+        const {isLoading, movies} = this.state;
+        return <section className="container">
+            {isLoading ? (<div className="loader">
+                <span className="loader__text">Loading...</span>
+            </div>) : (
+                <div className="movies">
+                    {movies.map(movie => {
+                        return<Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}/>
+                    })}
+                </div>
+            )}
+            </section>
+    }
 }
 
 export default App;
